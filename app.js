99a0d0bf715fa6621d1587e5cede8fbffd33d8ce
lps24 <105297290+lps24@users.noreply.github.com>
2022-05-10 15:34:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+let img1 = document.querySelector('#img1');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+
+btn1.addEventListener('click', () => {
+    img1.src = 'img/cokemodel.png';
+})
+
+btn2.addEventListener('click', () => {
+    img1.src = 'img/cokemodel1.png';
+})
+
+btn3.addEventListener('click', () => {
+    img1.src = 'img/cokemodel2.png';
+})
+
+let img2 = document.querySelector('#img2');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+let btn6 = document.querySelector('#btn6');
+
+btn4.addEventListener('click', () => {
+    img2.src = 'img/cokemodel3.png';
+})
+
+btn5.addEventListener('click', () => {
+    img2.src = 'img/cokemodel4.png';
+})
+
+btn6.addEventListener('click', () => {
+    img2.src = 'img/cokemodel5.png';
+})
+
+let img3 = document.querySelector('#img3');
+let btn7 = document.querySelector('#btn7');
+let btn8 = document.querySelector('#btn8');
+let btn9 = document.querySelector('#btn9');
+
+btn7.addEventListener('click', () => {
+    img3.src = 'img/cokemodel6.png';
+})
+
+btn8.addEventListener('click', () => {
+    img3.src = 'img/cokemodel7.png';
+})
+
+btn9.addEventListener('click', () => {
+    img3.src = 'img/cokemodel8.png';
+})
+
